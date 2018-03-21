9e67520f0f1a06244de9be73b24c83f4fb75e256
jeansung <intmain@hotmail.com>
2018-03-21 21:47:07 +0900

    ColorView 추가.

---
+++ RxColor/ColorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ColorViewController.swift
+//  RxColor
+//
+//  Created by leonard on 2018. 3. 21..
+//  Copyright © 2018년 Jeansung. All rights reserved.
+//
+
+import UIKit
+
+
+class ColorViewController: UIViewController {
+
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var graySlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
