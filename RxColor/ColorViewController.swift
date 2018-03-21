e99759f866bbf68deb8568c0e4b82803e09c290a
jeansung <intmain@hotmail.com>
2018-03-21 21:54:59 +0900

    slider 1개로 grayColor 지정하기.

--- RxColor/ColorViewController.swift
+++ RxColor/ColorViewController.swift
@@ -7,16 +7,29 @@
 //
 
 import UIKit
-
+import RxSwift
+import RxCocoa
 
 class ColorViewController: UIViewController {
 
     @IBOutlet weak var colorView: UIView!
     @IBOutlet weak var graySlider: UISlider!
+    var disposeBag: DisposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        bind()
     }
 
+}
+
+extension ColorViewController {
+    func bind() {
+        graySlider.rx.value
+            .map {
+                UIColor(white: CGFloat($0), alpha: 1)
+            }.subscribe(onNext: { [weak self] (color: UIColor) in
+                self?.colorView.backgroundColor = color
+            }).disposed(by: disposeBag)
+    }
 }
